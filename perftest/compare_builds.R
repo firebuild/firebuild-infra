571a25408f4ed4d8d5041908cae8e539968619ac
Balint Reczey <balint@balintreczey.hu>
2021-11-26 21:06:08 +0100

    perftest: Reindent compare_builds.R

--- perftest/compare_builds.R
+++ perftest/compare_builds.R
@@ -4,7 +4,7 @@
 
 args <- commandArgs(TRUE)
 if (length(args) != 3) {
-  stop("Usage: compare_builds.R <buildtimes.csv> <version 1> <version 2>", call.=FALSE)
+    stop("Usage: compare_builds.R <buildtimes.csv> <version 1> <version 2>", call.=FALSE)
 }
 
 buildtimes <- read.csv(args[1], header=FALSE, col.names=c("start", "end", "version", "ts", "name", "cmd", "status", "real1", "user1", "sys1", "real2", "user2", "sys2", "real3", "user3", "sys3", "cache.size.1", "cache.size.2"))
@@ -26,9 +26,9 @@ if (!all(dim(bt1) == dim(bt2))) {
 
 print(paste("Time % increase in", args[3], "vs.", args[2]))
 for(i in c(0, 1, 2)) {
- start_col <- 1 + i * 3
- end_col <- start_col + 2
- print(summary(100 * (bt2[, start_col:end_col] - bt1[, start_col:end_col]) / bt1[, start_col:end_col]))
+    start_col <- 1 + i * 3
+    end_col <- start_col + 2
+    print(summary(100 * (bt2[, start_col:end_col] - bt1[, start_col:end_col]) / bt1[, start_col:end_col]))
 }
 
 print(paste("Cache size % increase in", args[3], "vs.", args[2]))
